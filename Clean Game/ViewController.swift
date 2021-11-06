51fc512def5dc965b41966ec186d18f522ffc7e5
Yudha <setyajiyudha299@gmail.com>
2021-11-06 19:24:43 +0700

    Initial Commit

---
+++ Clean Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean Game
+//
+//  Created by Yudha Setyaji on 2021/11/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
